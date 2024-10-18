cc672e7bd1f7d8cf72be31db4527c05c0d2324ae
Alifseen <alifseen@gmail.com>
2024-10-18 18:27:24 +0500

    added endpoints for annual and all data

--- Section 29.py
+++ Section 29.py
@@ -38,6 +38,31 @@ def endpoint(station, date):
             "Date": date,
             "Temperature": temp}
 
+## 14. Add an endpoint for one station all values
+@app.route("/api/v1/<station>")
+def endpoint_all(station):
+    df = pd.read_csv(r"D:\Downloads\European_weather_data-all\TG_STAID" + station.zfill(6) + ".txt",
+                     skiprows=20,
+                     parse_dates=["    DATE"])
+    ## 15. Convert dataframe to dictionary since REST API only returns JSON (list and dictionaries)
+    results = df.to_dict(orient="records")  ## To make each row have its not dictionary, add "records"
+    return results
+
+
+## 16. Add an endpoint for one station one year
+@app.route("/api/v1/annual/<station>/<year>")  ## Since dynamically both this and the one station one date urls are same, we need to add "annual" to static part of the url
+def endpoint_annual(station, year):
+    ## 17. dont parse date, since we will use the str.startswith method to match the data
+    df = pd.read_csv(r"D:\Downloads\European_weather_data-all\TG_STAID" + station.zfill(6) + ".txt",
+                     skiprows=20)
+    ## 18. convert column to str and uses str method from series to use startswith string method to check the year after converting year int to str as well
+    df["    DATE"] = df["    DATE"].astype(str)  ## this series method converts datatype
+    results = df.loc[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return results
+
+
+
+
 
 ## 4. Run the app on port 5001 with debug on, only when this script is run.
 if __name__ == "__main__":
