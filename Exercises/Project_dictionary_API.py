dd53a21eae155fe5651b12995a36a426ecec3f02
Alifseen <alifseen@gmail.com>
2024-10-18 15:50:16 +0500

    Improvement to student project based on instructors instructions to keep static file out of function definition

--- Exercises/Project_dictionary_API.py
+++ Exercises/Project_dictionary_API.py
@@ -9,9 +9,12 @@ def main():
     return render_template("main.html")
 
 
+## Improvement by looking at instructors code: Instead of creating this df everything the url is hit, we create it only once since this is a static file and does not change, only when data changes, do you need to create a dataframe when endpoint is called.
+df = pd.read_csv("Files/dictionary.csv")
+
+
 @app.route("/api/v1/<word>")
 def api(word):
-    df = pd.read_csv("Files/dictionary.csv")
     definition = df.loc[df["word"] == word]["definition"].squeeze()
     return {
         "definition": definition,
