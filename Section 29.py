0955bf82c528c0415d06eb8b13b551c94d8ed9e9
Alifseen <alifseen@gmail.com>
2024-10-18 09:34:07 +0500

    Created a basic enpoint dictionary example to understand concept

--- Section 29.py
+++ Section 29.py
@@ -12,6 +12,15 @@ app = Flask(__name__)
 def home():
     return render_template("Home.html")
 
+
+## 5. Created an endpoint to return a dictionary with hardcoded values to understand APIs
+@app.route("/api/v1/<station>/<date>")  ## Dynamically get the station and date from url
+def endpoint(station, date):
+    temp = 24
+    return {"Station No.": station,
+            "Date": date,
+            "Temperature": temp}
+
 ## 4. Run the app on port 5001 with debug on, only when this script is run.
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
