093830136e6038944bb6a4b41ed70a980a18e5a2
Alifseen <alifseen@gmail.com>
2024-10-18 09:10:39 +0500

    Added notes on flask and created a basic weather data api home page

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather data API
+This project is about creating an API with a basic HTML Documentation page
--- Section 29.py
+++ Section 29.py
@@ -1,2 +1,2 @@
-"""Build a REST API with Documentation
+"""Build a REST API with an HTML Documentation Page
 """
